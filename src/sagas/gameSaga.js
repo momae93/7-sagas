563c724c29894980a3cc93545fee0c678a652433
Mickael AU <mickael.au@gmail.com>
2018-12-15 17:44:27 +0100

    [SAGAS] Handle life loss + score increment

--- src/sagas/gameSaga.js
+++ src/sagas/gameSaga.js
@@ -1,16 +1,21 @@
 import { call, put, takeEvery } from 'redux-saga/effects'
-import {delay} from 'redux-saga';
+import { delay } from 'redux-saga';
 
 function *handleGame(){
     yield put({type: 'GAME_START'});
     while (true){
-        yield call(delay, 1000);
+        yield call(delay, 3000);
         yield put({type: "TARGET_ADD_REQUESTED"});
     }
 }
 
+function *handleScoreIncrement(){
+    yield put({type: 'SCORE_INCREMENT'});
+}
+
 function *gameSaga(){
     yield takeEvery('GAME_START_REQUESTED', handleGame)
+    yield takeEvery('SCORE_INCREMENT_REQUESTED', handleScoreIncrement)
 }
 
 export default gameSaga;
--- src/sagas/targetSaga.js
+++ src/sagas/targetSaga.js
@@ -1,27 +1,40 @@
-import { put, takeEvery } from 'redux-saga/effects'
+import { call, put, takeEvery, select } from 'redux-saga/effects'
+import { delay } from 'redux-saga';
 
 //#region 
 
-function *handleIncrement(action){
-    yield put({type: 'INCREMENT_TARGET', id: action.id});
-}
-
 function *handleDecrement(action){
-    yield put({type: 'DECREMENT_TARGET', id: action.id});
+    const list = yield select(state => state.targets.list);
+    const target = list.find(x => x.id === action.id);
+
+    if (target != null){
+        if (target.value !== 0){
+            yield call(delay, 1000);
+            yield put({type: 'DECREMENT_TARGET', id: action.id});
+            yield put({type: 'TARGET_DECREMENT_REQUESTED', id: action.id});
+        }
+        else
+        {
+            yield put({type: 'DELETE_TARGET', id: action.id});
+            yield put({type: 'LIVES_DECREMENT'});
+        }
+    }
 }
 
 function *handleDelete(action){
     yield put({type: 'DELETE_TARGET', id: action.id});
+    yield put({type: 'SCORE_INCREMENT'})
 }
 
 function *handleAdd(){
-    yield put({type: 'ADD_TARGET'})
+    const id = yield select(state => state.targets.lastId);
+    yield put({type: 'ADD_TARGET', id: id + 1});
+    yield put({type: 'TARGET_DECREMENT_REQUESTED', id: id + 1})
 }
 
 //#endregion
 
 function *targetSaga(){
-    yield takeEvery('TARGET_INCREMENT_REQUEST', handleIncrement)
     yield takeEvery('TARGET_DECREMENT_REQUESTED', handleDecrement)
     yield takeEvery('TARGET_DELETE_REQUESTED', handleDelete)
     yield takeEvery('TARGET_ADD_REQUESTED', handleAdd)
